58cf66fac2a7a7dfad7a004e9d0afba439e99846
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-04-22 11:56:42 +0900

    インチからメートルの変換完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,7 +1,10 @@
 ﻿namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            
+            for (int inch = 1; inch <= 10; inch++) {
+                double meter = inch * 0.0254;
+                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+            }
         }
     }
 }
