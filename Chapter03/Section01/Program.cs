beb5cbb461871c0c743b72f1692cd89473762f8d
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-05-14 12:09:57 +0900

    デリケートによる実現

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -1,15 +1,26 @@
 ﻿namespace Section01 {
     internal class Program {
+
+        public delegate bool Judgement(int value);//デリゲートの宣言
+
         static void Main(string[] args) {
-            Console.Write("カウントしたい数値");
-            int count = int.Parse(Console.ReadLine());
-            Console.WriteLine(Count(count));
-        }
-        static int Count(int num) {
+            //Console.Write("カウントしたい数値");
+            //int num = int.Parse(Console.ReadLine());
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            Judgement judge = IsEven;
+            Console.WriteLine(Count(numbers,judge));
+        }
+
+        //メソッドへ渡す処理
+        static bool IsEven(int n) {
+            return n % 2 == 0;
+        }
+
+        static int Count(int[] numbers, Judgement judg) {
             var count = 0;
             foreach (var n in numbers) {
-                if (n == num) {
+                //引数で受け取ったメソッドを呼び出す
+                if (judg(n) == true) {
                     count++;
                 }
             }
