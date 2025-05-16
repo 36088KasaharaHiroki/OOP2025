87d6997fe60e589f1109531dbfc63ac6fa4ca403
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-05-16 14:56:57 +0900

    P71まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -1,21 +1,20 @@
-﻿namespace Section01 {
+﻿using System;
+
+namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            //Console.Write("カウントしたい数値");
-            //int num = int.Parse(Console.ReadLine());
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            Console.WriteLine(Count(numbers,n => 5 <= n && n < 10));
-        }
-
-        static int Count(int[] numbers, Predicate<int> judg) {
-            var count = 0;
-            foreach (var n in numbers) {
-                //引数で受け取ったメソッドを呼び出す
-                if (judg(n) == true) {
-                    count++;
-                }
-            }
-            return count;
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+            var lowerList = cities.ConvertAll(s => s.ToUpper());
+            lowerList.ForEach(s => Console.WriteLine(s));
         }
     }
 }
