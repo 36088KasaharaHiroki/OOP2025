23ceed3c2bb17e6c8da6f8b1fbd63a679fbc2933
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-05-23 15:08:19 +0900

    4-1-3

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
@@ -15,15 +17,41 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-            throw new NotImplementedException();
+            //foreach文
+            foreach (var s in langs) {
+                if (s.Contains('S'))
+                    Console.WriteLine(langs);
+            }
+            Console.WriteLine();
+
+            //for文
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S'))
+                    Console.WriteLine(langs[i]);
+            }
+            Console.WriteLine();
+
+            //while
+            int index = 0;
+            while (index < langs.Count) {
+                if (langs[index].Contains('S'))
+                    Console.WriteLine(langs[index]);
+                index++;
+            }
         }
 
         private static void Exercise2(List<string> langs) {
-            throw new NotImplementedException();
+            var selected = langs.Where(s => s.Contains('S'));
+            foreach (var lang in selected) {
+                Console.WriteLine(lang);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
-            throw new NotImplementedException();
+            var lang = langs.Find(s => s.Length >= 10);
+            Console.WriteLine(lang ?? "unknown");
+            //Console.WriteLine(langs.Find(s => s.Length >= 10) ?? "unknown");
+
         }
     }
 }
