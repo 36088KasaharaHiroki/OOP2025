7fd32368d59f305d53ae8e8afba4d92af9f73d61
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-06-04 11:27:57 +0900

    演習問題6-3-6

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -21,6 +21,38 @@ namespace Exercise03 {
             Console.WriteLine("6.3.5");
             Exercise5(text);
 
+            Console.WriteLine("6.3.6");
+            Exercise6(text);
+
+        }
+
+        private static void Exercise6(string text) {
+            //辞書で集計
+            var str = text.ToLower().Replace(" ","");           
+            var alphDicCount = Enumerable.Range('a', 26)
+                .ToDictionary(num => ((char)num).ToString(), num => 0);
+            foreach(var alph in str) {
+                alphDicCount[alph.ToString()]++;
+            }
+            foreach (var item in alphDicCount) {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+            Console.WriteLine();
+            //*********************************************************//
+            //配列で集計
+            var array = Enumerable.Repeat(0, 26).ToArray();
+            foreach(var alph in str) {
+                array[alph - 'a']++;
+            }
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                Console.WriteLine($"{ch}:{array[ch - 'a']}");
+            }
+            Console.WriteLine();
+            //*********************************************************//
+            //'a'から順にカウントして出力
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                Console.WriteLine($"{ch}:{text.Count(tc => tc == ch)}");
+            }
         }
 
         private static void Exercise1(string text) {
@@ -42,8 +74,6 @@ namespace Exercise03 {
                 sb.Append(word);
             }
             Console.WriteLine(sb + ".");
-
-
         }
 
         private static void Exercise4(string text) {
@@ -55,7 +85,6 @@ namespace Exercise03 {
             var count = text.Split(' ').Where(s => s.Length <= 4);
             foreach (var counts in count)
                 Console.WriteLine(counts);
-
         }
     }
 }
