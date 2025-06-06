f608def09a167fad9d116566fd4b82d0804c8c61
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-06-06 15:59:33 +0900

    演習問題5.1.3

---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+        public int Year { get; init; }
+        public int Month { get; init; }
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+        //5.1.2
+        public bool Is21Century => Year >= 2001 && Year <= 2100;
+        //5.1.3
+        public YearMonth AddOneMoth() {
+            if (Month < 12) {
+                return new YearMonth(Year, Month + 1);
+            } else {
+                return new YearMonth(Year + 1, Month - 11);
+            }
+        }
+        //5.1.4
+        public override string ToString() => 
+
+        
+        
+    }
+}
