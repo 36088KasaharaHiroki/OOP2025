1bbbe5835a945e047dbc9d90687fcc5345aaccff
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-06-17 11:28:07 +0900

    設問１_Studentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string Name { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public int Score { get; set; }
+    }
+}
