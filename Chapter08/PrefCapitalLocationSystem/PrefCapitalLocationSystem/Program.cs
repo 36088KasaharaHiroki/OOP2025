b1e053ab7385187ee944d46842c9d33799a7e519
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-06-17 09:45:37 +0900

    問題8-2準備

---
+++ Chapter08/PrefCapitalLocationSystem/PrefCapitalLocationSystem/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace PrefCapitalLocationSystem {
+    internal class Program {
+        static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
+        
+        //列挙型の定義
+        public  enum SelectMenu {
+            Alldisp = 1,
+            Search = 2,
+            Exit = 9
+        }
+
+
+
+        static void Main(string[] args) {
+            String? pref, prefCaptalLocation;
+
+            //入力処理
+            Console.WriteLine("県庁所在地の登録【入力終了：Ctrl + 'Z'】");
+
+            while (true) {
+                //①都道府県の入力
+                Console.Write("都道府県:");
+                pref = Console.ReadLine();
+
+                if (pref == null) break;    //無限ループを抜ける(Ctrl + 'Z')
+
+                //県庁所在地の入力
+                Console.Write("県庁所在地:");
+                prefCaptalLocation = Console.ReadLine();
+
+                //既に都道府県が登録されているか？
+                //ヒント：ContainsKeyを使用して調べる
+                if (prefOfficeDict.ContainsKey(pref)) {
+                    Console.WriteLine("上書きしますか？(Y/N)");
+                    if (Console.ReadLine() == "N") continue;
+                }
+                //登録済みなら確認して上書き処理、上書きしない場合はもう一度都道府県の入力…①へ
+                //ヒント：Console.WriteLine("上書きしますか？(Y/N)");
+
+                //*　ここに入力　*******************//
+
+
+                //県庁所在地登録処理
+
+                //*　ここに入力　*******************//
+                prefOfficeDict[pref] = prefCaptalLocation ?? "******";
+
+
+                Console.WriteLine();//改行
+            }
+
+            Boolean endFlag = false;    //終了フラグ（無限ループを抜け出す用）
+            while (!endFlag) {
+                switch (menuDisp()) {
+                    case SelectMenu.Alldisp:       //一覧出力処理
+                        break;
+                    case SelectMenu.Search:        //検索処理
+                        searchPrefCaptalLocation();
+                        break;
+                    case SelectMenu.Exit:          //無限ループを抜ける
+                        endFlag = true;
+                        break;
+                }
+            }
+        }
+
+        //メニュー表示
+        private static SelectMenu menuDisp() {
+            Console.WriteLine("\n**** メニュー ****");
+            Console.WriteLine("1：一覧表示");
+            Console.WriteLine("2：検索");
+            Console.WriteLine("9：終了");
+            Console.Write(">");
+            var menuSelect = (SelectMenu)int.Parse(Console.ReadLine());
+            return menuSelect;
+        }
+
+
+        //一覧表示処理
+        private static void allDisp() {
+            //*　ここに入力　*******************//
+            foreach(var (key,value) in prefOfficeDict) {
+                Console.WriteLine($"{key}の県庁所在地は{value}です。");
+            }
+        }
+
+        //検索処理
+        private static void searchPrefCaptalLocation() {
+            Console.Write("都道府県:");
+            String? searchPref = Console.ReadLine();
+            //*　ここに入力　*******************//
+            if (searchPref is null) return;
+            Console.WriteLine(searchPref + "の県庁所在地は" + prefOfficeDict[searchPref] + "です");
+        }
+    }
+}
---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            var Dict = new Dictionary<char, int>();
+            foreach (var ch in text.ToUpper()) {
+                if ('A'<= ch && ch <= 'Z') {
+                    Dict[ch]++;
+                } else {
+                    Dict[ch] = 1;
+                }
+                    
+            }
+            foreach (var item in Dict.OrderBy(x => x.Key)) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+
+        }
+
+        private static void Exercise2(string text) {
+            var dict = new SortedDictionary<Char, int>();
+
+            foreach (var uc in text.ToUpper()) {
+                if ('A' <= uc && uc <= 'Z') {
+                    if (dict.ContainsKey(uc))
+                        dict[uc]++;     //  登録済み：valueをインクリメント
+                    else
+                        dict[uc] = 1;   //  未登録：valueに1を設定
+                }
+            }
+            //⑥すべての文字が読み終わったら出力
+            foreach (var item in dict) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
+        }
+    }
+}
