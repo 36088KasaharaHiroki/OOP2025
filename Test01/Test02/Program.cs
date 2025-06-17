a0729c2a27698438c889c5e38b961b144b3f4697
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-06-17 12:07:39 +0900

    設問２-４

--- Test01/Test02/Program.cs
+++ Test01/Test02/Program.cs
@@ -75,28 +75,34 @@ namespace Test02 {
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(int[] numbers) {
-
+            var num = numbers.Select(n => n);
+            foreach (var n in num) {
+                Console.Write($"{n} ");
+            }
 
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(int[] numbers) {
-            
+            var num = numbers.Where(n => n >= 10 && n <= 50);
+            foreach (var n in num) {
+                Console.Write($"{n} ");
+            }
 
         }
 
         //問題５　Countメソッドを使い、小文字の'n'が含まれている都市名がいくつあるかカウントして結果を表示
         //　　　　出力結果【5】
         private static void Exercise05(List<string> cities) {
-            
+
 
         }
 
         //問題６　全都市数
         //　　　　出力結果【8】
         private static void Exercise06(List<string> cities) {
-            
+
 
         }
 
@@ -111,7 +117,7 @@ namespace Test02 {
         //          Paris
         //          Tokyo】
         private static void Exercise07(List<string> cities) {
-            
+
 
         }
 
@@ -126,7 +132,7 @@ namespace Test02 {
         //　　　　  Canberra : 8文字
         //　　　　  Hong Kong : 9文字】
         private static void Exercise08(List<string> cities) {
-            
+
 
 
 
@@ -143,7 +149,7 @@ namespace Test02 {
         //          New Delhi : 9文字
         //          Hong Kong : 9文字】
         private static void Exercise09(List<string> cities) {
-            
+
 
 
 
@@ -154,7 +160,7 @@ namespace Test02 {
         //        【London
         //          Berlin】
         private static void Exercise10(List<string> cities) {
-            
+
 
 
         }
