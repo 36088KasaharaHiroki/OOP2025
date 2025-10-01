66f3372946963baa0ba30bd783767b90e2af957e
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-10-01 11:30:54 +0900

    Converter UI

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -37,23 +37,25 @@ namespace ColorChecker {
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
             //colorAreaの色（背景色）は、スライダーで指定したRGBの色を表示する
             //colorArea.Background = new SolidColorBrush(Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value));
-            currentColor = new MyColor {Color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value),
-                Name = ((MyColor[])DataContext).Where(c=>
-                       c.Color.R == (byte)rSlider.Value 
-                    && c.Color.G == (byte)gSlider.Value 
-                    && c.Color.B == (byte)bSlider.Value).Select(x=>x.Name).FirstOrDefault()
+            currentColor = new MyColor { Color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value),
+                Name = ((MyColor[])DataContext).Where(c =>
+                       c.Color.R == (byte)rSlider.Value
+                    && c.Color.G == (byte)gSlider.Value
+                    && c.Color.B == (byte)bSlider.Value).Select(x => x.Name).FirstOrDefault()
+            
             };
             colorArea.Background = new SolidColorBrush(currentColor.Color);
-            colorSelectComboBox.SelectedIndex = GetColorToIndex(currentColor.Color);       
+            colorSelectComboBox.SelectedIndex = GetColorToIndex(currentColor.Color);
         }
 
         private void stockButton_Click(object sender, RoutedEventArgs e) {
-            if (!stockList.Items.Contains(currentColor)) {
+            if (!stockList.Items.Contains((MyColor)currentColor)) {
                 //stockList.Items.Add("R：" + rSlider.Value + "  G：" + gSlider.Value + "  B：" + bSlider.Value);
                 stockList.Items.Insert(0, currentColor);
             } else {
                 MessageBox.Show("既に登録済みです！","ColorCherker", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
+
         }
 
         private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
