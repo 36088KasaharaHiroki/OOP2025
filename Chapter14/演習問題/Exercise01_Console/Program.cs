452288685b4086e29667259a2c59a95236f509d9
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-11-05 10:56:43 +0900

    演習14-1

---
+++ Chapter14/演習問題/Exercise01_Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise01_Console {
+    internal class Program {
+        static void Main(string[] args) {
+            public async Task Example() {
+                var filePath = "走れメロス.txt";
+                await foreach (var line in File.ReadLinesAsync(filePath) {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    } 
+}
---
+++ Chapter14/演習問題/Exercise01_WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise01_WPF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private async Task Button_Click(object sender, RoutedEventArgs e) {
+            TextArea.Text = await TextReaderSample.ReadTextAsync("走れメロス.txt");
+        }
+    }
+    static class TextReaderSample {
+        public static async Task<string> ReadTextAsync(string filePath) {
+            var sb = new StringBuilder();
+            var sr = new StringReader(filePath);
+            //while (!sr.EndOfStream) {
+                var line = await sr.ReadLineAsync();
+                sb.AppendLine(line);
+                await Task.Delay(2);
+            //}
+            return sb.ToString();
+        }
+    }
+}
