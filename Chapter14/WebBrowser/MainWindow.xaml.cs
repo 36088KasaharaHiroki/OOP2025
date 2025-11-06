432375f38daa52da5d81c754bcd5506a36a92bb4
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-11-06 14:42:23 +0900

    WebBrowser完成

---
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WebBrowser;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        InitializeAsync();
+    }
+
+    private async void InitializeAsync() {
+        await WebView.EnsureCoreWebView2Async();
+        WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStarting;
+        WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+    }    
+
+    private void CoreWebView2_NavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e) {
+        LodingBar.Visibility = Visibility.Visible;
+        LodingBar.IsIndeterminate = true;
+    }
+
+    private void CoreWebView2_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e) {
+        LodingBar.Visibility = Visibility.Visible;
+        LodingBar.IsIndeterminate = false;
+    }
+
+    private void BackButton_Click(object sender, RoutedEventArgs e) {
+        if (WebView.CanGoBack) {
+            WebView.GoBack();
+        }
+    }
+
+    private void FowardButton_Click(object sender, RoutedEventArgs e) {
+        if (WebView.CanGoForward) {
+            WebView.GoForward();
+        }
+    }
+
+    private void GoButton_Click(object sender, RoutedEventArgs e) {
+        var url = AddressBar.Text.Trim();
+        if (string.IsNullOrWhiteSpace(url)) return;
+        WebView.Source = new Uri(url);
+    }
+}
