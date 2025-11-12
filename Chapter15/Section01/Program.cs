e43d3dfaa87731de94980d0376b7b4709ebe3fe2
KasaharaHiroki <oibc36088@ojs.ac.jp>
2025-11-12 10:13:02 +0900

    継承を使ったポリモーフィズム

---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<GreetingBase> list = [
+                new GreetingMorning(),
+                new GreetingAfternon(),
+                new GreetingEvening(),
+            ];
+
+            foreach (var obj in list) {
+                string msg = obj.GetMessage();
+                Console.WriteLine(msg);
+            }
+        }
+    }
+
+    class GreetingMorning : GreetingBase {
+        public override string GetMessage() => "おはよう";
+    }
+
+    class GreetingAfternon : GreetingBase {
+        public override string GetMessage() => "こんにちは";
+    }
+
+    class GreetingEvening : GreetingBase {
+        public override string GetMessage() => "こんばんは";
+    }
+
+}
